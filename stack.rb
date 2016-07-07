22fa127db54c238e1128baeb2a8c4f5ce26becb5
Calvin <Calvin.lacc@gmail.com>
2016-07-07 15:09:52 -0600

    Finished the stack ruby file

---
+++ stack.rb
@@ -0,0 +1,49 @@
+class Stack 
+	attr_accessor :top, :memory_array
+
+	def initialize
+		@memory_array = []
+		@length = @memory_array.length
+	end 
+
+	def push(thing_to_be_pushed)
+		@memory_array[@length] = thing_to_be_pushed
+		@length +=1 
+	end
+
+	def pop
+		return if !empty?
+		
+		length = @memory_array.length
+		@memory_array[length-1] = nil
+		@length -= 1
+	end
+
+	def empty?
+		return @length == 0
+	end 
+
+	def peek
+		return @memory_array[@length-1]
+	end
+
+
+	def displayer
+		puts "Currently inside the array is"
+		print "#{@memory_array}"
+	end
+
+end
+
+s = Stack.new 
+
+s.push("hihi")
+s.push("lol")
+s.push("bye")
+s.pop 
+s.push("new")
+s.push("loool")
+s.displayer
+puts s.peek
+
+
